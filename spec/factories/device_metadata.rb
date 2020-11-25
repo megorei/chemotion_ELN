9928ea502e77d53b7bb9cb970e014ebbf5853753
Thomas Watolla <tw@megorei.com>
2020-11-25 01:35:16 +0100

    Add spec & factory

---
+++ spec/factories/device_metadata.rb
@@ -0,0 +1,32 @@
+FactoryBot.define do
+  factory :device_metadata do
+    name { 'Device Metadata' }
+    sequence(:doi) { |i| "10.12345/DEVICE-#{i}" }
+    url { "http://doi.org/#{doi}" }
+    landing_page { url }
+    type {}
+    description { 'Metadata for device'}
+    publisher { 'Chemotion' }
+    publication_year { Time.current.year }
+    owners {[{
+      owner: {
+        ownerName: Faker::Company.name,
+        ownerContact: Faker::Internet.email,
+        ownerIdentifier: {}
+      }
+    }]}
+    manufacturers {[{
+      manufacturer: {
+        manufacturerName: Faker::Company.name,
+        modelName: 'TES-T 123',
+        manufacturereIdentifier: {}
+      }
+    }]}
+    dates {[{
+      date: {
+        date: '2020-11-11',
+        dateType: 'Commissioned'
+      }
+    }]}
+end
+end
