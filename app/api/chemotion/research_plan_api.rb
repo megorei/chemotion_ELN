813ae87a9f828bcfd5f24200fa2047e6ba6685ca
Thomas Watolla <tw@megorei.com>
2021-01-15 18:28:41 +0100

    Initialize empty metadata object

--- app/api/chemotion/research_plan_api.rb
+++ app/api/chemotion/research_plan_api.rb
@@ -120,9 +120,10 @@ module Chemotion
         end
         get do
           research_plan = ResearchPlan.find(params[:id])
+          research_plan.build_research_plan_metadata(title: ResearchPlan.name) if research_plan.research_plan_metadata.nil?
           {
             research_plan: ElementPermissionProxy.new(current_user, research_plan, user_ids).serialized,
-            attachments: Entities::AttachmentEntity.represent(research_plan.attachments)
+            attachments: Entities::AttachmentEntity.represent(research_plan.attachments),
           }
         end
       end
