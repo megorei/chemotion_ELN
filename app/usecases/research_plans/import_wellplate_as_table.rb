22fbff774675399f0303716c7c033b0a62efef56
Johannes Haubold <jh@megorei.com>
2022-01-21 16:00:34 +0100

    Move wellplate exporter to research plan namespace

--- app/usecases/research_plans/import_wellplate_as_table.rb
+++ app/usecases/research_plans/import_wellplate_as_table.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module Usecases
-  module Wellplates
-    class ExportToResearchPlan
+  module ResearchPlans
+    class ImportWellplateAsTable
       attr_reader :research_plan, :wellplate
 
       def initialize(research_plan, wellplate)
