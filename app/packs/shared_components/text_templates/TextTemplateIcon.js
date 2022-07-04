a5ede35e84b4169038f37ef2e3c42920a98e19c7
Johannes Haubold <jh@megorei.com>
2022-07-04 15:41:15 +0200

    Move text_templates components to shared_components

---
+++ app/packs/shared_components/text_templates/TextTemplateIcon.js
@@ -0,0 +1,32 @@
+// imports from node_modules
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TextTemplateIcon = ({ template }) => {
+  if (!template) return <span />;
+
+  const { data, name } = template;
+
+  if (data.icon) {
+    return (
+      <i className={data.icon} />
+    );
+  }
+
+  const text = (data || {}).text || name;
+
+  return (
+    <span>{text.toUpperCase()}</span>
+  );
+};
+
+TextTemplateIcon.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  template: PropTypes.object
+};
+
+TextTemplateIcon.defaultProps = {
+  template: null
+};
+
+export default TextTemplateIcon;
