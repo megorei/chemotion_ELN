c554dd4d184dea2d97e560135ab4568217614318
Beate Quednau <beate@firma-pflaum.de>
2025-01-10 15:39:39 +0100

    Add macromolecule to api

--- app/api/chemotion/code_log_api.rb
+++ app/api/chemotion/code_log_api.rb
@@ -49,7 +49,8 @@ module Chemotion
       namespace :print_codes do
         desc 'Build PDF with element bar & qr code'
         params do
-          requires :element_type, type: String, values: %w[sample reaction wellplate screen device_description]
+          requires :element_type, type: String,
+                                  values: %w[sample reaction wellplate screen device_description macromolecule]
           # TODO: check coerce with  type Array[Integer] not working with before do
           requires :ids, type: Array # , coerce_with: ->(val) { val.split(/,/).map(&:to_i) }
           requires :width, type: Integer
@@ -98,7 +99,8 @@ module Chemotion
       namespace :print_analyses_codes do
         desc 'Build PDF with analyses codes of one analysis type'
         params do
-          requires :element_type, type: String, values: %w[sample reaction wellplate screen device_description]
+          requires :element_type, type: String,
+                                  values: %w[sample reaction wellplate screen device_description macromolecule]
           requires :id, type: Integer, desc: 'Element id'
           requires :analyses_ids, type: Array[String]
           requires :size, type: String, values: %w[small big]
