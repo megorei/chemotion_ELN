af658c904feefa4ebe641b6fabe96712736e40e0
Beate Quednau <bq@megorei.com>
2025-01-14 19:10:23 +0100

    Add macromolecule to components, models, config

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -89,6 +89,7 @@ class Profile < ApplicationRecord
                   'research_plan' => 5,
                   'cell_line' => -1000,
                   'device_description' => -1100,
+                  'macromolecule' => -1200,
                 })
   end
 end
