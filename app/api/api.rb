35d4a04d11f2c2a2d01625cae870fa9a8b4555f7
Johannes Haubold <jh@megorei.com>
2025-03-03 16:31:23 +0100

    Current working copy

--- app/api/api.rb
+++ app/api/api.rb
@@ -211,6 +211,7 @@ class API < Grape::API
   mount Chemotion::VersionAPI
   mount Chemotion::ComponentAPI
   mount Chemotion::VesselAPI
+  mount Chemotion::SequenceBasedMacromoleculeAPI
 
   if Rails.env.development?
     add_swagger_documentation(info: {
