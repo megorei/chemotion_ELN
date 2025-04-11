519a2a3f11e9353b803537ef87ff67c0b5f39bcc
Johannes Haubold <jh@megorei.com>
2025-04-11 12:30:50 +0200

    Finish renaming and fix typo

--- app/api/entities/user_entity.rb
+++ app/api/entities/user_entity.rb
@@ -13,7 +13,7 @@ module Entities
     expose :reactions_count, documentation: { type: 'Integer', desc: 'Reactions count' }
     expose :cell_lines_count, documentation: { type: 'Integer', desc: 'Cellline Samples count' }
     expose :device_descriptions_count, documentation: { type: 'Integer', desc: 'Device Descriptions count' }
-    expose :sequence_based_macromolecules_count, documentation: { type: 'Integer', desc: 'Sequence-based macromolecules count' }
+    expose :sequence_based_macromolecule_samples_count, documentation: { type: 'Integer', desc: 'Sequence-based macromolecule samples count' }
     expose :type, if: ->(obj, _opts) { obj.respond_to? :type }
     expose :reaction_name_prefix, if: ->(obj, _opts) { obj.respond_to? :reaction_name_prefix }
     expose :layout, if: ->(obj, _opts) { obj.respond_to? :layout }
@@ -47,7 +47,7 @@ module Entities
     end
 
     def sequence_based_macromolecules_count
-      object.counters['sequence_based_macromolecules'].to_i
+      object.counters['sequence_based_macromolecule_samples'].to_i
     end
 
     expose :current_sign_in_at do |obj|
