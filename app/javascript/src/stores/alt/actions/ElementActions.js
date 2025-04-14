da713c5925baba8b8d98a2e699e88cceb77aa100
Beate Quednau <bq@megorei.com>
2025-04-14 14:32:09 +0200

    Add sbmm element in frontend (#24)
    * Add macromolecule to api
    
    * Add macromolecule to components, models, config
    
    * Add basic fetcher and javascript model
    
    * Add macromolecule to stores
    
    * Rename new element to sequence_based_macromolecule
    
    * Add basic detail components
    
    * Use helper functions for eln elements and captialize words
    
    * Add sequence based macromolecule detail forms, mobx store, fixes
    
    * Fix number field, elements in api
    
    * Fix multiple open and changed sequence based macromolecules
    
    * Add basic search result modal, Fix unit field
    
    * Add basic calculation
    
    * Add different dummy search results
    
    * Add dropzone and droparea, cleanup
    
    * Fix comments
    
    * Change logic of form visibility
    
    * Add analyses tab content
    
    * Move calculation to javascript model
    
    * Add common comment button and comment box
    
    * Add attachment form
    
    * Cleanup
    
    * Change to renamed model
    
    * Rename to sequence based macromolecule sample
    
    * Fix detail level name
    
    * Rename sample fields
    
    * Rename sbmm field names
    
    * Add disabled to form
    
    * Remove identifier
    
    * Fix stores and api for sbmm naming
    
    * Fixes
    
    * Add search for eln and uniprot
    
    * Fix toggle buttons, missing fields
    
    * Add validation, fix list filter
    
    * Fix error messages and toggable contents
    
    * Fix reference drag n drop
    
    * Fixes for details header and dnd
    
    * Fix units
    
    * Add basic list grouped by sbmm
    
    * Add drag handle for sbmm
    
    * Prepare for update
    
    * Empty value if checkbox unchecked
    
    * Fixes
    
    * Fixes
    
    * Rename to sequence based macromolecule sample
    
    * Show short name in list
    
    * Fix sample calculation
    
    * Remove temporary structure details
    
    * Fix update sbmm sample
    
    * Remove split and copy buttons
    
    * Fix collection tab

--- app/javascript/src/stores/alt/actions/ElementActions.js
+++ app/javascript/src/stores/alt/actions/ElementActions.js
@@ -21,6 +21,7 @@ import GenericElsFetcher from 'src/fetchers/GenericElsFetcher';
 import PrivateNoteFetcher from 'src/fetchers/PrivateNoteFetcher'
 import MetadataFetcher from 'src/fetchers/MetadataFetcher';
 import DeviceDescriptionFetcher from 'src/fetchers/DeviceDescriptionFetcher';
+import SequenceBasedMacromoleculeSamplesFetcher from 'src/fetchers/SequenceBasedMacromoleculeSamplesFetcher';
 
 import GenericEl from 'src/models/GenericEl';
 import Sample from 'src/models/Sample';
@@ -36,6 +37,7 @@ import Graph from 'src/models/Graph';
 import ComputeTask from 'src/models/ComputeTask';
 import LiteratureMap from 'src/models/LiteratureMap';
 import Prediction from 'src/models/Prediction';
+import SequenceBasedMacromoleculeSample from 'src/models/SequenceBasedMacromoleculeSample';
 import ReactionSvgFetcher from 'src/fetchers/ReactionSvgFetcher';
 import Metadata from 'src/models/Metadata';
 import UserStore from 'src/stores/alt/stores/UserStore';
@@ -339,6 +341,7 @@ class ElementActions {
         });
     };
   }
+
   fetchCellLinesByCollectionId(id, queryParams = {}, collectionIsSync = false) {
     return (dispatch) => {
       CellLinesFetcher.fetchByCollectionId(id, queryParams, collectionIsSync)
@@ -353,6 +356,17 @@ class ElementActions {
   fetchDeviceDescriptionsByCollectionId(id, queryParams = {}, collectionIsSync = false) {
     return (dispatch) => {
       DeviceDescriptionFetcher.fetchByCollectionId(id, queryParams, collectionIsSync)
+        .then((result) => {
+          dispatch(result);
+        }).catch((errorMessage) => {
+          console.log(errorMessage);
+        });
+    };
+  }
+
+  fetchSequenceBasedMacromoleculeSamplesByCollectionId(id, queryParams = {}, collectionIsSync = false) {
+    return (dispatch) => {
+      SequenceBasedMacromoleculeSamplesFetcher.fetchByCollectionId(id, queryParams, collectionIsSync)
         .then((result) => {
           dispatch(result);
         }).catch((errorMessage) => {
@@ -492,9 +506,9 @@ class ElementActions {
     return cellLineSample;
   }
 
-  copyCellLineFromId(id,collectionId ) {
+  copyCellLineFromId(id, collectionId) {
     return (dispatch) => {
-      CellLinesFetcher.copyCellLine(id,collectionId)
+      CellLinesFetcher.copyCellLine(id, collectionId)
         .then((result) => {
           dispatch(result);
         }).catch((errorMessage) => {
@@ -703,19 +717,19 @@ class ElementActions {
     };
   }
 
-    splitAsSubCellLines(ui_state) {
-      return (dispatch) => {
-        const ids = ui_state["cell_line"].checkedIds.toArray();
-        const collection_id = ui_state.currentCollection.id;
+  splitAsSubCellLines(ui_state) {
+    return (dispatch) => {
+      const ids = ui_state["cell_line"].checkedIds.toArray();
+      const collection_id = ui_state.currentCollection.id;
         
-        CellLinesFetcher.splitAsSubCellLines(ids,collection_id)
-          .then((result) => {
-            dispatch(ui_state);
-          }).catch((errorMessage) => {
-            console.log(errorMessage);
-          });
-      };
-    }
+      CellLinesFetcher.splitAsSubCellLines(ids, collection_id)
+        .then((result) => {
+          dispatch(ui_state);
+        }).catch((errorMessage) => {
+          console.log(errorMessage);
+        });
+    };
+  }
 
   bulkCreateWellplatesFromSamples(params) {
     let { collection_id, samples, wellplateCount } = params;
@@ -987,6 +1001,60 @@ class ElementActions {
   splitAsSubDeviceDescription(ui_state) {
     return (dispatch) => {
       DeviceDescriptionFetcher.splitAsSubDeviceDescription(ui_state)
+        .then((result) => {
+          dispatch(ui_state.ui_state);
+        }).catch((errorMessage) => {
+          console.log(errorMessage);
+        });
+    };
+  }
+
+  // -- Sequence Based Macromolecule Samples --
+
+  fetchSequenceBasedMacromoleculeSampleById(id) {
+    return (dispatch) => {
+      SequenceBasedMacromoleculeSamplesFetcher.fetchById(id)
+        .then((result) => {
+          dispatch(result);
+        }).catch((errorMessage) => {
+          console.log(errorMessage);
+        });
+    };
+  }
+
+  updateSequenceBasedMacromoleculeSample(params) {
+    return (dispatch) => {
+      SequenceBasedMacromoleculeSamplesFetcher.updateSequenceBasedMacromoleculeSample(params)
+        .then((result) => {
+          dispatch(result);
+        }).catch((errorMessage) => {
+          console.log(errorMessage);
+        });
+    };
+  }
+
+  generateEmptySequenceBasedMacromoleculeSample(collection_id) {
+    return SequenceBasedMacromoleculeSample.buildEmpty(collection_id);
+  }
+
+  createSequenceBasedMacromoleculeSample(params) {
+    return (dispatch) => {
+      SequenceBasedMacromoleculeSamplesFetcher.createSequenceBasedMacromoleculeSample(params)
+        .then((result) => {
+          dispatch(result);
+        }).catch((errorMessage) => {
+          console.log(errorMessage);
+        });
+    };
+  }
+
+  copySequenceBasedMacromoleculeSampleFromClipboard(collection_id) {
+    return collection_id;
+  }
+
+  splitAsSubSequenceBasedMacromoleculeSample(ui_state) {
+    return (dispatch) => {
+      SequenceBasedMacromoleculeSamplesFetcher.splitAsSubSequenceBasedMacromoleculeSample(ui_state)
         .then((result) => {
           dispatch(ui_state.ui_state);
         }).catch((errorMessage) => {
