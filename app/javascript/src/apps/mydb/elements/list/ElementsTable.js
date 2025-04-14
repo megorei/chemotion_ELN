17175d356e3ed1c9ae1af8b72d957b82b83be472
Beate Quednau <bq@megorei.com>
2025-04-14 14:32:09 +0200

    Add sbmm element in frontend (#24)
    * Add macromolecule to api
    
    * Add macromolecule to components, models, config
    
    * Add basic fetcher and javascript model
    
    * Add macromolecule to stores
    
    * Rename new element to sequence_based_macromolecule
    
    * Add basic detail components
    
    * Use helper functions for eln elements and captialize words
    
    * Add sequence based macromolecule detail forms, mobx store, fixes
    
    * Fix number field, elements in api
    
    * Fix multiple open and changed sequence based macromolecules
    
    * Add basic search result modal, Fix unit field
    
    * Add basic calculation
    
    * Add different dummy search results
    
    * Add dropzone and droparea, cleanup
    
    * Fix comments
    
    * Change logic of form visibility
    
    * Add analyses tab content
    
    * Move calculation to javascript model
    
    * Add common comment button and comment box
    
    * Add attachment form
    
    * Cleanup
    
    * Change to renamed model
    
    * Rename to sequence based macromolecule sample
    
    * Fix detail level name
    
    * Rename sample fields
    
    * Rename sbmm field names
    
    * Add disabled to form
    
    * Remove identifier
    
    * Fix stores and api for sbmm naming
    
    * Fixes
    
    * Add search for eln and uniprot
    
    * Fix toggle buttons, missing fields
    
    * Add validation, fix list filter
    
    * Fix error messages and toggable contents
    
    * Fix reference drag n drop
    
    * Fixes for details header and dnd
    
    * Fix units
    
    * Add basic list grouped by sbmm
    
    * Add drag handle for sbmm
    
    * Prepare for update
    
    * Empty value if checkbox unchecked
    
    * Fixes
    
    * Fixes
    
    * Rename to sequence based macromolecule sample
    
    * Show short name in list
    
    * Fix sample calculation
    
    * Remove temporary structure details
    
    * Fix update sbmm sample
    
    * Remove split and copy buttons
    
    * Fix collection tab

--- app/javascript/src/apps/mydb/elements/list/ElementsTable.js
+++ app/javascript/src/apps/mydb/elements/list/ElementsTable.js
@@ -28,6 +28,8 @@ import VesselTemplateGroupView from 'src/apps/mydb/elements/list/vessel/VesselTe
 import ChevronIcon from 'src/components/common/ChevronIcon';
 import DeviceDescriptionList from 'src/apps/mydb/elements/list/deviceDescriptions/DeviceDescriptionList';
 import DeviceDescriptionListHeader from 'src/apps/mydb/elements/list/deviceDescriptions/DeviceDescriptionListHeader';
+import SequenceBasedMacromoleculeSampleList from 'src/apps/mydb/elements/list/sequenceBasedMacromoleculeSamples/SequenceBasedMacromoleculeSampleList';
+import SequenceBasedMacromoleculeSampleHeader from 'src/apps/mydb/elements/list/sequenceBasedMacromoleculeSamples/SequenceBasedMacromoleculeSampleHeader';
 
 export default class ElementsTable extends React.Component {
   constructor(props) {
@@ -616,6 +618,8 @@ export default class ElementsTable extends React.Component {
       );
     } else if (type === 'cell_line') {
       typeSpecificHeader = this.collapseButton();
+    } else if (type === 'sequence_based_macromolecule_sample') {
+      typeSpecificHeader = <SequenceBasedMacromoleculeSampleHeader elements={elements} />;
     } else if (genericEl) {
       typeSpecificHeader = this.renderGenericElementsHeader();
     } else if (type === 'vessel') {
@@ -725,6 +729,14 @@ export default class ElementsTable extends React.Component {
           />
         );
       }
+    } else if (type === 'sequence_based_macromolecule_sample') {
+      elementsTableEntries = (
+        <SequenceBasedMacromoleculeSampleList
+          elements={elements}
+          currentElement={currentElement}
+          ui={ui}
+        />
+      );
     } else {
       elementsTableEntries = (
         <ElementsTableEntries
