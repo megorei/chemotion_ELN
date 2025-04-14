e4655f9954f9f6124aa4822b62ef2328cfcfdfc2
Beate Quednau <bq@megorei.com>
2025-04-14 14:32:09 +0200

    Add sbmm element in frontend (#24)
    * Add macromolecule to api
    
    * Add macromolecule to components, models, config
    
    * Add basic fetcher and javascript model
    
    * Add macromolecule to stores
    
    * Rename new element to sequence_based_macromolecule
    
    * Add basic detail components
    
    * Use helper functions for eln elements and captialize words
    
    * Add sequence based macromolecule detail forms, mobx store, fixes
    
    * Fix number field, elements in api
    
    * Fix multiple open and changed sequence based macromolecules
    
    * Add basic search result modal, Fix unit field
    
    * Add basic calculation
    
    * Add different dummy search results
    
    * Add dropzone and droparea, cleanup
    
    * Fix comments
    
    * Change logic of form visibility
    
    * Add analyses tab content
    
    * Move calculation to javascript model
    
    * Add common comment button and comment box
    
    * Add attachment form
    
    * Cleanup
    
    * Change to renamed model
    
    * Rename to sequence based macromolecule sample
    
    * Fix detail level name
    
    * Rename sample fields
    
    * Rename sbmm field names
    
    * Add disabled to form
    
    * Remove identifier
    
    * Fix stores and api for sbmm naming
    
    * Fixes
    
    * Add search for eln and uniprot
    
    * Fix toggle buttons, missing fields
    
    * Add validation, fix list filter
    
    * Fix error messages and toggable contents
    
    * Fix reference drag n drop
    
    * Fixes for details header and dnd
    
    * Fix units
    
    * Add basic list grouped by sbmm
    
    * Add drag handle for sbmm
    
    * Prepare for update
    
    * Empty value if checkbox unchecked
    
    * Fixes
    
    * Fixes
    
    * Rename to sequence based macromolecule sample
    
    * Show short name in list
    
    * Fix sample calculation
    
    * Remove temporary structure details
    
    * Fix update sbmm sample
    
    * Remove split and copy buttons
    
    * Fix collection tab

--- app/javascript/src/apps/mydb/elements/details/ElementDetails.js
+++ app/javascript/src/apps/mydb/elements/details/ElementDetails.js
@@ -20,6 +20,10 @@ import ScreenDetails from 'src/apps/mydb/elements/details/screens/ScreenDetails'
 import UserStore from 'src/stores/alt/stores/UserStore';
 import WellplateDetails from 'src/apps/mydb/elements/details/wellplates/WellplateDetails';
 import CellLineDetails from 'src/apps/mydb/elements/details/cellLines/CellLineDetails';
+import SequenceBasedMacromoleculeSampleDetails from 'src/apps/mydb/elements/details/sequenceBasedMacromoleculeSamples/SequenceBasedMacromoleculeSampleDetails';
+import {
+  Tabs, Tab, Button, Badge
+} from 'react-bootstrap';
 
 const tabInfoHash = {
   metadata: {
@@ -163,6 +167,8 @@ export default class ElementDetails extends Component {
         return <LiteratureDetails literatureMap={el} />;
       case 'cell_line':
         return <CellLineDetails cellLineItem={el} />;
+      case 'sequence_based_macromolecule_sample':
+        return <SequenceBasedMacromoleculeSampleDetails />;
       default:
         return (
           <div style={{ textAlign: 'center' }}>
